e68645ad49dd34583985f9c0c3e85413fb7226ee
SudarshanRaoP <sudarshan.visham188@gmail.com>
2016-05-13 13:04:15 +0530

    Ad-hoc Ansible

--- install-R.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ -z "$1" ]];then
-    echo "No hostgroup provided."
-    exit
-else
-    ansible $1 -m yum -a "name=epel-release state=latest"
-    ansible $1 -m yum -a "name=R state=present"
-fi
